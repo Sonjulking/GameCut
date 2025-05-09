f867f9bc5ce5c50d9c73140441edcb27bef7c47b
jeonhj1015 <mumu097@naver.com>
2025-05-09 12:37:14 +0900

    test

--- src/main/java/com/gamecut/action/MyPageAction.java
+++ src/main/java/com/gamecut/action/MyPageAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.gamecut.dao.FileDAO;
 import com.gamecut.dao.UserDAO;
@@ -15,7 +16,9 @@ public class MyPageAction implements GameCutAction {
 	@Override
 	public String pro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		int userNo = 1;
+		HttpSession session = request.getSession();
+		UserVO vo = (UserVO) session.getAttribute("loginUSER");
+		int userNo = vo.getUserNo();
 		UserDAO userDao = new UserDAO();
 		FileDAO fileDao = new FileDAO();
 		request.setAttribute("user", userDao.selectUser(userNo));
