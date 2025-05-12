b8c0bc33077428dfb4f03e2b61675ab75fe10a25
sjw042635 <jwsung10@naver.com>
2025-05-12 15:07:10 +0900

    [feat] 댓글기능추가, 포인트기능추가_250512

---
+++ src/main/java/com/gamecut/action/UpdateCommentAction.java
@@ -0,0 +1,45 @@
+package com.gamecut.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gamecut.dao.CommentDAO;
+import com.gamecut.vo.CommentVO;
+import com.gamecut.vo.UserVO;
+
+public class UpdateCommentAction implements GameCutAction {
+
+	@Override
+	public String pro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+	    UserVO loginUser = (UserVO) session.getAttribute("loginUSER");
+
+	    if (loginUser == null) {
+	        response.sendRedirect("login.do");
+	        return null;
+	    }
+
+	    int commentNo = Integer.parseInt(request.getParameter("commentNo"));
+	    String content = request.getParameter("commentContent");
+
+	    // 댓글 작성자 확인
+	    CommentVO original = new CommentDAO().getCommentByNo(commentNo);
+	    if (original == null || original.getUserNo() != loginUser.getUserNo()) {
+	        response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 권한 없음
+	        return null;
+	    }
+
+	    CommentVO vo = new CommentVO();
+	    vo.setCommentNo(commentNo);
+	    vo.setCommentContent(content);
+
+	    new CommentDAO().updateComment(vo);
+	    return "commentResult.jsp";
+	}
+
+}
