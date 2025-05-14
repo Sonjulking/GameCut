db8941eaa7adcc833cf276ef04029e39f0e51cd4
kkc <bushwick97@naver.com>
2025-05-14 13:55:49 +0900

    fix2

--- src/main/java/com/gamecut/action/ItemShopOKAction.java
+++ src/main/java/com/gamecut/action/ItemShopOKAction.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.gamecut.dao.ItemDAO;
+import com.gamecut.dao.UserDAO;
 
 public class ItemShopOKAction implements GameCutAction{
 
@@ -39,6 +41,10 @@ public class ItemShopOKAction implements GameCutAction{
 
         // 실제 구매 처리
         dao.purchaseItem(userNo, itemNo);
+
+        HttpSession session = request.getSession();
+        UserDAO userDao = new UserDAO();
+        session.setAttribute("loginUSER", userDao.selectUser(userNo));
 
         // 성공 리턴
         response.setContentType("text/plain; charset=UTF-8");
